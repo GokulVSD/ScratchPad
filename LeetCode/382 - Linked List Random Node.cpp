4eea0bcf83c7ce54b2712b67c7991b41f9c74a4f
GokulVSD <gokul.vasda@gmail.com>
2020-09-27 18:03:39 +0530

    Solved some more problems

---
+++ LeetCode/382 - Linked List Random Node.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/linked-list-random-node/
+
+// This was way too easy. It's the fastest solution in C++ according to LeetCode.
+
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    ListNode* head;
+    size_t size = 1;
+    
+    Solution(ListNode* head) {
+        srand(time(0));
+        this->head = head;
+        while(head->next != NULL){
+            head = head->next;
+            ++(this->size);
+        } 
+        head->next = this->head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        size_t x = rand() % this->size;
+        while(x--)
+            this->head = this->head->next;
+        return this->head->val;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
---
+++ LeetCode/705 - Design HashSet.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/design-hashset/
+
+// Seriously, needed stricter constraints. Faster than 99.5% of solutions.
+
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+    bool hs[1000000];
+    MyHashSet() {
+        fill(this->hs, this->hs + 1000000, false);
+    }
+    
+    void add(int key) {
+        this->hs[key] = true;
+    }
+    
+    void remove(int key) {
+        this->hs[key]= false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return this->hs[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
