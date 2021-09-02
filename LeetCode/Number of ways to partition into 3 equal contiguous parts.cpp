2acac6e716f4788679f38b71c01f8babcc17bddd
Gokul Vasudeva <gokul.vasda@gmail.com>
2021-09-02 13:42:42 +0530

    Create Number of ways to partition into 3 equal contiguous parts.cpp

---
+++ LeetCode/Number of ways to partition into 3 equal contiguous parts.cpp
@@ -0,0 +1,9 @@
+// Count the number of ways to divide an array into three contiguous parts having equal sum
+
+Create prefix array
+
+Create 2 arrays left and right
+Starting from left or right, put true to all positions which
+Sum to total/3
+
+Use these two arrays, and the prefix array to count number of ways
